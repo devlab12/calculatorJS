45373c0e1575e36a52ed43f41da56e2d6edf034e
devlab12 <alexander@MacBook-Pro-Alexander.local>
2015-04-06 22:26:34 +0300

    All the functionality was implemented + General refactoring
    Operators don’t work properly yet. Should be fixed.
    This is an interim commit.

--- calculator.js
+++ calculator.js
@@ -2,15 +2,23 @@
 // Functionality block
 // =======
 
-
+// This keeps the object of a display block
 var displayScreen = document.getElementById("calcDisplay");
+
+// This is needed to keep a previous value and an operator for calculations
+var operationType = false;
+var previousValue = 0;
+
+
+// === EVENTS HANDLING ===
 
 // Reset button click event
 document.getElementById("reset").addEventListener("click", function () {
     displayScreen.innerHTML = "0";
+    previousValue = 0;
 });
 
-// Bacspace button event
+// Backspace button event
 document.getElementById("backspace").addEventListener("click", function () {
     if (displayScreen.innerHTML.length > 1) {
         displayScreen.innerHTML = displayScreen.innerHTML.substr(0, displayScreen.innerHTML.length - 1);
@@ -34,6 +42,18 @@ document.getElementById("num0").addEventListener("click", numberClick);
 // Dot mouse click event handling
 document.getElementById("numDot").addEventListener("click", dotClick);
 
+// Operator selection event handling
+document.getElementById("plus").addEventListener("click", operatorClick);
+document.getElementById("minus").addEventListener("click", operatorClick);
+document.getElementById("multiply").addEventListener("click", operatorClick);
+document.getElementById("divide").addEventListener("click", operatorClick);
+
+// Sum operator click event handling
+document.getElementById("sum").addEventListener("click", operatorSum);
+
+
+// === FUNCTIONALITY IMPLEMENTATION ===
+
 // Number click functionality implementation   
 function numberClick(){  
     var currentNum = this.innerHTML;
@@ -56,24 +76,44 @@ function dotClick(){
     };
 };
 
-
-/*
-// Plus event
-document.getElementById("plus").onclick = function() {
-    console.log('plus event');
+// Operator click functionality implementation
+function operatorClick(){
+    operationType = this.innerHTML;
+    
+    if (operationType == "+"){
+        previousValue += +displayScreen.innerHTML;
+    }else if(operationType == "-"){
+        previousValue -= +displayScreen.innerHTML;
+    }else if(operationType == "/"){
+        previousValue /= +displayScreen.innerHTML;
+    }else{
+        // multiply operator goes the last to avoid the comparison with its symbol
+        previousValue *= +displayScreen.innerHTML;
+    };
+    
+    // Clearing the display to collect different variables to be operated
+    displayScreen.innerHTML = "0";
+    
+    console.log("previous value is " + previousValue);
+    console.log(operationType);
 };
 
-document.getElementById("minus").addEventListener("click", function() {
-    console.log('minus event');
-});
-
-//document.getElementById("sum").onclick = newScript();
+// Sum operator click functionality implementation
+function operatorSum(){
+    if (operationType == "+"){
+         displayScreen.innerHTML = +displayScreen.innerHTML + previousValue;
+    }else if(operationType == "-"){
+        displayScreen.innerHTML = +displayScreen.innerHTML - previousValue;
+    }else if(operationType == "/"){
+        displayScreen.innerHTML = +displayScreen.innerHTML / previousValue;
+    }else{
+        // multiply operator goes the last to avoid the comparison with its symbol
+        displayScreen.innerHTML = +displayScreen.innerHTML * previousValue;
+    };
     
-function newScript(){
-    console.log('It works');
-}
-
-*/
+    console.log("previous value is " + previousValue);
+    console.log(operationType);
+};
 
 // =======
 // Visualisation block
